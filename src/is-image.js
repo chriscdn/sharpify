8423eee4b8159e962c62ad8e618345cad4563a8a
Christopher Meyer <chris@schwiiz.org>
2020-04-29 15:25:58 +0200

    fixing dependencies

--- src/is-image.js
+++ src/is-image.js
@@ -1,7 +1,7 @@
 const readChunk = require('read-chunk')
 const imageType = require('image-type')
 
-module.exports = async function isJPG(filePath) {
+module.exports = async function(filePath) {
 	try {
 		const buffer = await readChunk(filePath, 0, 12)
 		const type = imageType(buffer)
