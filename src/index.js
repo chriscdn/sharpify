5dfe014554c65445e6f6d9c7dbce065636784e8b
Christopher Meyer <chris@schwiiz.org>
2020-04-29 23:18:39 +0200

    improvements

--- src/index.js
+++ src/index.js
@@ -1,48 +1,7 @@
-const {
-	fork
-} = require('child_process')
+const sharpify = require('./sharpify')
+const isImage = require('./is-image')
 
-const pathExists = require('path-exists')
-const Semaphore = require('@chriscdn/promise-semaphore')
-
-const lock = new Semaphore()
-
-const defaultArgs = {
-	blur: 0,
-	saturation: 1,
-	width: null,
-	height: null,
-	fit: 'inside',
-	rotate: null
-}
-
-module.exports = async function(source, target, args) {
-
-	let jargs = {
-		source,
-		target,
-		...defaultArgs,
-		...args
-	}
-
-	// only one process should ever write to target at a time
-	await lock.acquire(target)
-
-	let f = fork(__dirname + '/_sharpify.js', [JSON.stringify(jargs)])
-
-	return new Promise((resolve, reject) => {
-			f.on('exit', async code => {
-				if (await pathExists(target)) {
-					resolve()
-				} else {
-					reject(new Error('Sharpify: An unknown error occurred.'))
-				}
-
-			})
-			f.on('error', err => reject(err))
-		})
-		.finally(() => {
-			lock.release(target)
-		})
-
+module.exports = {
+	sharpify,
+	isImage
 }
