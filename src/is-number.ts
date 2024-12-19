a69eae25e871fa7ee691cc2a3235a84df9e2b1e7
Christopher Meyer <chris@schwiiz.org>
2024-12-19 13:18:08 +0100

    2.0.4

--- src/is-number.ts
+++ src/is-number.ts
@@ -8,9 +8,9 @@
 export const isNumber = (num: unknown): num is number => {
     if (typeof num === "number") {
         return num - num === 0;
-    }
-    if (typeof num === "string" && num.trim() !== "") {
+    } else if (typeof num === "string" && num.trim() !== "") {
         return Number.isFinite ? Number.isFinite(+num) : isFinite(+num);
+    } else {
+        return false;
     }
-    return false;
 };
